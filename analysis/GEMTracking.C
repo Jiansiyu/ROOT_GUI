607c074fef8c7faa9deeecdf04c35807f0e1c39c
Jiansiyu <jiansiyu@gmail.com>
2018-10-02 21:03:00 -0400

    test

--- analysis/GEMTracking.C
+++ analysis/GEMTracking.C
@@ -309,13 +309,13 @@ void GEMTracking::Run(Int_t event, const char *filename)
 		    mCluster[cluster.Module].push_back(cluster);
 		}
 		std::cout<<__FUNCTION__<<"["<<__LINE__<<"]"<<"how many chamber fired"<<mCluster.size()<<std::endl;
-		getchar();
+
 
 		FillHistograms(); //up to here, all hits and clusters in one entry have been filled to vHit and vCluster.
 		std::cout<<__FUNCTION__<<"["<<__LINE__<<"] "<< i<<std::endl;
 		std::cout<<__FUNCTION__<<"["<<__LINE__<<"] "<< i<<std::endl;
 		std::cout<<__FUNCTION__<<"["<<__LINE__<<"] ************************"<< i<<std::endl<<std::endl;
-
+		getchar();
 	}
 
 	PrintHistograms(filename);
