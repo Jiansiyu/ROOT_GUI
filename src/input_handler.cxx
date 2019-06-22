9226f420941dfac0c5ab39d81bcd70d8e92a4a07
Siyu <jiansiyu@gmail.com>
2019-06-21 20:29:03 -0400

    change the name of the pedestal to the prex standared, changed the unordered mapping to ordered mapping

--- src/input_handler.cxx
+++ src/input_handler.cxx
@@ -6,7 +6,7 @@
 #include <iostream>
 
 #include <cmath>
-#include "unordered_map"
+#include "map"
 
 //_______________________________________________
 InputHandler::InputHandler()
@@ -553,8 +553,8 @@ int InputHandler::SummPedestal()
    int ChNb[128] ={1, 33, 65, 97, 9, 41, 73, 105, 17, 49, 81, 113, 25, 57, 89, 121, 3, 35, 67, 99, 11, 43, 75, 107, 19, 51, 83, 115, 27, 59, 91, 123, 5, 37, 69, 101, 13, 45, 77, 109, 21, 53, 85, 117, 29, 61, 93, 125, 7, 39, 71, 103, 15, 47, 79, 111, 23, 55, 87, 119, 31, 63, 95, 127, 0, 32, 64, 96, 8, 40, 72, 104, 16, 48, 80, 112, 24, 56, 88, 120, 2, 34, 66, 98, 10, 42, 74, 106, 18, 50, 82, 114, 26, 58, 90, 122, 4, 36, 68, 100, 12, 44, 76, 108, 20, 52, 84, 116, 28, 60, 92, 124, 6, 38, 70, 102, 14, 46, 78, 110, 22, 54, 86, 118, 30, 62, 94, 126};//this is original and believed to be the right one,Jan 09 2017
 
           //planeID     dimension          plane-strips value
-   std::map<int,std::map<int,std::unordered_map<int,float>>> prex_mean_array;
-   std::map<int,std::map<int,std::unordered_map<int,float>>> prex_rms_array;
+   std::map<int,std::map<int,std::map<int,float>>> prex_mean_array;
+   std::map<int,std::map<int,std::map<int,float>>> prex_rms_array;
 
    //convert all the value into the real order
    for(auto mpd_iter=prex_mean.begin();mpd_iter!=prex_mean.end();mpd_iter++){
@@ -588,9 +588,9 @@ int InputHandler::SummPedestal()
 
 		   std::string _mean_header;
 		   if(dimension_iter->first){  // if 1,Y dimension
-			   _mean_header=Form("prex.gems.y%d.mean = \\\n",plane_iter->first);
+			   _mean_header=Form("prex.gems.y%d.ped = \\\n",plane_iter->first);
 		   }else{
-			   _mean_header=Form("prex.gems.x%d.mean = \\\n",plane_iter->first);
+			   _mean_header=Form("prex.gems.x%d.ped = \\\n",plane_iter->first);
 		   }
 		   pfilio<<_mean_header;
 
@@ -606,7 +606,7 @@ int InputHandler::SummPedestal()
 				   temp_counter=0;
 			   }
 		   }
-		   pfilio<<"]\n";
+		   pfilio<<"\n";
 
 		   // write the rms value to the file
 		   std::string _rms_header;
@@ -631,7 +631,7 @@ int InputHandler::SummPedestal()
 				   temp_counter=0;
 			   	}
 		   }
-		   pfilio<<"]\n";
+		   pfilio<<"\n";
 
 	   }
    }
